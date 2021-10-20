301dcd9b3796047c00285b1479f054deae9a82d1
Hechen Liu <codingchallenge4@gmail.com>
2021-10-20 00:04:14 -0400

    catch errors

--- playlist-to-files.ps1
+++ playlist-to-files.ps1
@@ -1,48 +1,57 @@
-﻿param([string]$path = "C:\Users\codin\Downloads\playlist\new")
+﻿param([string]$path, [string]$destination="$path\files")
 
-if (Test-Path $path) {
-    $playlists = @(Get-ChildItem -Path $path -Filter "*.m3u8")
-    if ($playlists.length -eq 0) {
-        Write-Host "No playlists found"
-    }
 
-    foreach ($playlist in $playlists) {
-        $endIndex = $playlist.Name.LastIndexOf(".m3u8")
-        $name = $playlist.Name.Substring(0, $endIndex)
+Add-Type -AssemblyName System.Web
 
-        $newPath = "$path\files\$name"
-        if (!(Test-Path $newPath)) {
-            New-Item -Path "$path\files" -Name $name -ItemType "directory"            
+try {
+    if (Test-Path $path) {
+        $playlists = @(Get-ChildItem -Path $path -Filter "*.m3u8")
+        if ($playlists.length -eq 0) {
+            Write-Host "No playlists found"
         }
 
-        $content = $playlist | Get-Content -Encoding utf8
-        $pattern = "^file:\/\/\/(?<location>[A-Z]:\/.+\.mp3)"
+        foreach ($playlist in $playlists) {
+            $endIndex = $playlist.Name.LastIndexOf(".m3u8")
+            $name = $playlist.Name.Substring(0, $endIndex)
+
+            $newPath = "$destination\$name"
+            if (!(Test-Path $newPath)) {
+                New-Item -Path "$destination" -Name $name -ItemType "directory"            
+            }
+
+            $content = $playlist | Get-Content -Encoding utf8
+            $pattern = "^file:\/\/\/(?<location>[A-Z]:\/.+\.mp3)"
     
-        $notEmpty = $false
-        $missingFiles = 0
-        foreach($line in $content) {
-            $requests = $line -match $pattern
-            if ($requests) {
-                $notEmpty = $true
-                $file = [System.Web.HTTPUtility]::UrlDecode($Matches.location)
-                if (Test-Path $file) {
-                    Copy-Item -Path $file -Destination $newPath
+            $notEmpty = $false
+            $missingFiles = 0
+            foreach($line in $content) {
+                $requests = $line -match $pattern
+                if ($requests) {
+                    $notEmpty = $true
+                    $file = [System.Web.HTTPUtility]::UrlDecode($Matches.location)
+                    if (Test-Path $file) {
+                        Copy-Item -Path $file -Destination $newPath
+                    } else {
+                        $missingFiles++
+                    }
+                }
+            }
+            if (!($notEmpty)) {
+                Write-Host "Playlist $name : Empty playlist"
+            } else {
+                if ($missingFiles -eq 0) {
+                    Write-Host "Playlist $name : Success"
+                    # previous files will be overwritten
                 } else {
-                    $missingFiles++
+                    Write-Host "Playlist $name : $missingFiles files cannot be found"
                 }
             }
         }
-        if (!($notEmpty)) {
-            Write-Host "$name : Empty playlist"
-        } else {
-            if ($missingFiles -eq 0) {
-                Write-Host "$name : Success"
-                # previous files will be overwritten
-            } else {
-                Write-Host "$name : $missingFiles files cannot be found"
-            }
-        }
+    } else {
+        Write-Host "No such path found"
     }
-} else {
-    Write-Host "No such path found"
+}
+catch {
+  Write-Host "An error occurred:"
+  Write-Host $_
 }
